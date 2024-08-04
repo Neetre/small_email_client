67ada0f963218c66ae2571d3b119b682a9846558
19746 <mattiabraga2006@gmail.com>
2024-08-04 12:39:48 +0200

    Read email, auto response, all ok

--- bin/main.py
+++ bin/main.py
@@ -7,6 +7,7 @@ Neetre 2024
 import os
 import getpass
 from dotenv import load_dotenv
+load_dotenv()
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
@@ -14,16 +15,17 @@ from langchain_groq import ChatGroq
 from read_email import read_email
 from send_email import send_email
 
-load_dotenv()
+
 EMAIL_ACCOUNT = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
 GROQ_API_KEY=os.environ["GROQ_API_KEY"]
 
 
-def auto_response(message):
+def auto_response(body):
+    message = f"Respond to this email's body: {body}"
     chat = ChatGroq(temperature=1, groq_api_key=GROQ_API_KEY, model_name="llama-3.1-70b-versatile")
 
-    system = "You respond to the email"
+    system = "Respond to the email as if you were a person writing a response."
     human = "{text}"
     prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
     chain = prompt | chat
@@ -33,17 +35,50 @@ def auto_response(message):
     return text_response
 
 
-def elab():
-    pass
+def show_emails(emails: list):
+    for email in emails:
+        yield email
+
 
 def main():
     mailbox = 'inbox'
     filter = 'UNSEEN'
-    respond_to = read_email()
-    for email in respond_to:
-        to_email, message = elab(email)
-        text_response = auto_response(message)
-        send_email(text_response)
+    email_filter = "FROM ''"
+    emails = read_email(mailbox, filter, email_filter)
+    print(len(emails), "emails found!")
+    if len(emails) == 0:
+        print("No emails found!")
+        return
+
+    email_generator = show_emails(emails)
+    while True:
+        try:
+            email = next(email_generator)
+            print("\nID:", email["ID"])
+            print("From:", email["From"])
+            print("Subject:", email["Subject"])
+            print("Date:", email["Date"])
+            print("Body:", email["Body"])
+
+            next_email = input("\nDo you want to see the next email? (y/n): ")
+            if next_email.lower() == 'n':
+                break
+        except StopIteration:
+            print("No more emails!")
+            break
+
+    email_id = input("Enter the email id to respond to: ")
+    email = next((e for e in emails if e["ID"] == email_id.encode()), None)
+    if email is None:
+        print("Email not found!")
+        return
+    body = email["Body"]
+    to_email = email["From"]
+
+    text_response = auto_response(body)
+    print("Response:", text_response)
+    # send_email(to_email, text_response)
+    print("Email sent!")
 
 
 if __name__ == "__main__":
--- bin/read_email.py
+++ bin/read_email.py
@@ -9,26 +9,27 @@ IMAP_SERVER = 'imap.gmail.com'
 EMAIL_ACCOUNT = os.environ.get('EMAIL')
 PASSWORD = os.environ.get('PASSWORD')
 
-
 def smtp_server(email):
     domain = email.split("@")[1]
     domain = "smtp." + domain
     return domain
 
 
-def read_email(mailbox='inbox', filter='UNSEEN'):
-    data = []
+def read_email(mailbox='inbox', filter='UNSEEN', email_filter=None) -> list:
+    emails = []
     server = smtp_server(EMAIL_ACCOUNT)
     mail = imaplib.IMAP4_SSL(server)
     mail.login(EMAIL_ACCOUNT, PASSWORD)
 
     mail.select(mailbox)
-    status, email_ids = mail.search(None, filter)
+    search_filter = filter if email_filter is None else email_filter
+    status, email_ids = mail.search(None, search_filter)
     email_ids = email_ids[0].split()
     
     for e_id in email_ids:
         _, data = mail.fetch(e_id, "(RFC822)")
-        email_message = email.message_from_bytes(data[0][1])
+        raw_email = data[0][1]
+        email_message = email.message_from_bytes(raw_email)
         
         # print(f"Message Number: {e_id}")
         # print(f"From: {email_message.get('From')}")
@@ -38,7 +39,7 @@ def read_email(mailbox='inbox', filter='UNSEEN'):
         # print(f"Subject: {email_message.get('Subject')}")
         
         msg = {
-            "Message Number": e_id,
+            "ID": e_id,
             "From": email_message.get("From"),
             "To": email_message.get("To"),
             "BCC": email_message.get("BCC"),
@@ -47,7 +48,6 @@ def read_email(mailbox='inbox', filter='UNSEEN'):
             "Body": ""
         }
 
-        print("Content: ")
         if email_message.is_multipart():
             for part in email_message.walk():
                 content_type = part.get_content_type()
@@ -60,12 +60,12 @@ def read_email(mailbox='inbox', filter='UNSEEN'):
             content_type = email_message.get_content_type()
             body = email_message.get_payload(decode=True).decode()
             msg["Body"] = body
-        data.append(msg)
+        emails.append(msg)
 
     mail.close()
     mail.logout()
-
-    return data
+    print(f"{len(emails)} emails found!")
+    return emails
 
     
 def main():
